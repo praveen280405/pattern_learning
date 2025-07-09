051d067649d7f3c5c1e383197169cac8d1677a43
praveen280405 <156937418+praveen280405@users.noreply.github.com>
2025-07-09 15:47:18 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -33,4 +33,41 @@ class Main {
 * * 
 * * * 
 * * * * 
-
+//box:
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        pattern1(5);
+    }
+    static void pattern1(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+*****
+*****
+*****
+*****
+*****
+    
+class Main {
+    public static void main(String[] args) {
+        pattern4(4);
+    }
+    static void pattern4(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++ ){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
+1
+12
+123
+1234
